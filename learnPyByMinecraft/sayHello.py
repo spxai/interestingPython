a0041ce02e0876487ba0f9b96c8dfb26165e2322
spxai <lx@myhaspl.com>
2021-05-05 18:26:47 +0800

    Update sayHello.py

--- learnPyByMinecraft/sayHello.py
+++ learnPyByMinecraft/sayHello.py
@@ -3,25 +3,16 @@
 
 # 引入mcpi目录下的mcpi.minecraft
 import mcpi.minecraft as minecraft
-import datetime
-import time
+
 
 # 建立mc连接对象
 mc = minecraft.Minecraft.create("192.168.10.9")
 
 
 # 发送消息
-now = datetime.datetime.now()
-nowStr=now.strftime("%Y/%m/%d %H:%M:%S")
 
-
-
-
-
-while True:
-    # 获得玩家位置
-    pos = mc.player.getTilePos()       
-    mc.postToChat(f"今天是：{nowStr}[思普兴智能] ，您的位置是({pos.x},{pos.y},{pos.z})")
+while True:    
+    mc.postToChat("你好，世界！")
     time.sleep(5)
 
 
