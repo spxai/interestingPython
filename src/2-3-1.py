6ff3327ccb4f41d215fd2029d2400b9a4eb02d2e
spxai <lx@myhaspl.com>
2021-05-08 11:14:43 +0800

    Create 2-3-1.py

---
+++ src/2-3-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+import random
+rndNum=random.random()
+print(rndNum)
+rndNum=random.random()
+print(rndNum)
+rndNum=random.random()
+print(rndNum)
