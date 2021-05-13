644a150e84400d78f76c2639e2b3bcc207935571
spxai <lx@myhaspl.com>
2021-05-13 22:01:10 +0800

    Update 2-5-1.py

--- src/2-5-1.py
+++ src/2-5-1.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #2-5-1.py
+import datetime
+now=datetime.datetime.now()
+name=input("输入名字:")
+birthYear=input("您是哪一年出生的?")
+age=now.year-int(birthYear)+1
+print(f"{name},您今年虚岁:{age}")
