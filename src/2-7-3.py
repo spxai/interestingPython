7fddf003cedb0a4c7d13011fa8c38dd757c5ff64
spxai <lx@myhaspl.com>
2021-05-26 21:29:34 +0800

    Create 2-7-3.py

---
+++ src/2-7-3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2-7-3.py
+import turtle
+turtle.color('Green','yellow')
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(75)
+print('按回车键退出')
+input()
