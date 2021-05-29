b23591fe0bd83b701447d01d79f70ea48fe4290b
spxai <lx@myhaspl.com>
2021-05-29 22:42:36 +0800

    ct

---
+++ src/2-7-5-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2-7-5-1.py
+import turtle
+turtle.home()
+turtle.bk(300)
+turtle.home()
+turtle.fd(300)
+turtle.home()
+turtle.lt(90)
+turtle.fd(300)
+turtle.home()
+turtle.rt(90)
+turtle.fd(300)
+turtle.home()
+turtle.color('green')
+turtle.circle(120)
+turtle.home()
+turtle.color('red')
+turtle.circle(-60)
+turtle.up()
+turtle.color('blue')
+turtle.goto(0,-120)
+turtle.down()
+turtle.circle(120)
+print('按回车键退出')
+input()
---
+++ src/2-7-6.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2-7-4.py
+import turtle
+
+lineLen=90
+ts = turtle.getscreen()
+ts.colormode(255)
+for b in range(0,256):
+    turtle.color(0,0,b)
+    turtle.fd(lineLen)
+    posX,posY=turtle.position()
+    turtle.goto(posX, posY+1)
+    lineLen=-lineLen
+ts.colormode(1)
+print('按回车键退出')
+input()
