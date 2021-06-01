1359bf579db99b654ed44304d26ada10007f3440
spxai <lx@myhaspl.com>
2021-06-01 11:48:28 +0800

    Update 2-7-7.py

--- src/2-7-7.py
+++ src/2-7-7.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#2-7-4.py
+#2-7-7.py
 import turtle
-
-lineLen=90
-ts = turtle.getscreen()
-ts.colormode(255)
-for b in range(0,256):
-    turtle.color(0,0,b)
-    turtle.fd(lineLen)
-    posX,posY=turtle.position()
-    turtle.goto(posX, posY+1)
-    lineLen=-lineLen
-ts.colormode(1)
+turtle.color('Green','yellow')
+while True:
+    turtle.forward(200)
+    turtle.left(150)
+    print(turtle.pos())
+    if abs(turtle.pos()) < 1:
+        break
 print('按回车键退出')
 input()
