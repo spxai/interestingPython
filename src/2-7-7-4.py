dba23bfe723c83fd8d456e686a46e596ef30b714
spxai <lx@myhaspl.com>
2021-06-05 15:30:52 +0800

    ud

---
+++ src/2-7-7-4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2-7-7-4.py
+x=0
+while x <10:
+    print(x,end=" ")
+    x+=1
---
+++ src/2-7-7-5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2-7-7-5.py
+x=0
+while x <10:
+    print(x,end=" ")
+    x+=1
+else:
+    print()
+    print(x,"x---the output end---")
+
+for y in range(10):
+    print(y,end=" ")
+else:
+    print()
+    print(y,"y---the output end---")   
