3d689a60be2f09a35c450aa2cbc566dd4275c6a8
spxai <lx@myhaspl.com>
2021-06-13 19:25:47 +0800

    Update 2-7-11.py

--- src/2-7-11.py
+++ src/2-7-11.py
@@ -3,13 +3,12 @@
 #2-7-11.py
 import turtle
 turtle.color('Green','yellow')
-turtle.home()
-lineLine=100
-for i in range((360//30)):
-    turtle.setpos(0,0)
-    turtle.forward(lineLine+i*10)
-    turtle.left(30)  
-    turtle.write(f"{i*30}>[{turtle.position()[0]},\n{turtle.position()[1]}]") 
+lineLine=50
+for i in range(0,361,30):
+    turtle.home()
+    turtle.left(i)  
+    turtle.forward(lineLine+i)  
+    turtle.write(f"{i}>[{turtle.position()[0]},\n{turtle.position()[1]}]") 
 turtle.home()
 turtle.color('Black','red')
 turtle.write(f"[{turtle.position()[0]},\n{turtle.position()[1]}]") 
