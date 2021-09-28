c1c9ae5aaf648bb49c4f1d0afc186eedb47962dc
spxai <lx@myhaspl.com>
2021-09-28 21:43:24 +0800

    Create ElecBook.py

---
+++ src/2-4-3/ElecBook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2-4-3-ElecBook.py
+import Book
+class ElecBook(Book):
+    """电子书管理"""
+    def __init__(self,name,author,price,webName):
+        super.__init__(name,author,price)
+        self.webName="XX电子书超市"
+        self.clickCount=0
+    def getInfo(self):
+        return f"书名:{self.name},作者:{self.author},价格:{self.price},售卖网站名:{self.webName},+点击次数{self.clickCount}"
+    def clickBook():
+        self.clickCount+=1
+        
