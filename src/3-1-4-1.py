65567ccf9d90b897fa9139f12c83cf0ad337d84c
spxai <msp@myhaspl.com>
2021-12-14 19:47:10 +0800

    aa

--- src/3-1-4-1.py
+++ src/3-1-4-1.py
@@ -8,14 +8,22 @@ import time
 pygame.init()
 #sys.path[0]获取当前工作目录
 pygame.display.set_caption("图片大小变更")
-logo = pygame.image.load(sys.path[0]+"/pic/3-1-4-1/game1.png")
+logo = pygame.image.load(sys.path[0]+r"\pic\3-1-4\game1.png")
 pygame.display.set_icon(logo)
-bjpic = pygame.image.load(sys.path[0]+"/pic/3-1-4-1/grounds.png")
+bjpic = pygame.image.load(sys.path[0]+r"\pic\3-1-4\bj.jpg")
 bjpicRect = bjpic.get_rect()
 size = width, height =bjpicRect.width*10,bjpicRect.height*10
 screen = pygame.display.set_mode(size)
 bjpic=pygame.Surface.convert(bjpic)
-ball = pygame.image.load(sys.path[0]+"/pic/3-1-4-1/game2.png")
+ball = pygame.image.load(sys.path[0]+r"\pic\3-1-4\game2.png")
 ball=pygame.Surface.convert(ball)
 ballrect = ball.get_rect()
-        
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.blit(bjpic,(0,0)) 
+    pygame.display.update()
+    time.sleep(1)
